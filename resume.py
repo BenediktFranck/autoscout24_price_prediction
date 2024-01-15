35b221d5d924be28e924e48f8687eb6e921e5e1a
BenediktFranck <150929764+BenediktFranck@users.noreply.github.com>
2024-01-15 17:24:56 +0100

    Update resume.py

--- resume.py
+++ resume.py
@@ -23,8 +23,8 @@ def app():
         # --- PATH SETTINGS ---
         #current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
         #css_file = current_dir / "styles" / "main.css"
-        resume_file = 'assets/cv.pdf'
-        profile_pic = 'assets/profile-pic.png'
+        resume_file = 'cv/Benedikt_Franck.pdf'
+        profile_pic = 'cv/profile-pic.png'
      
         Path(__file__).parents[1] / 'GarretBurhennData/Garret_Burhenn_Pitches.csv'
         
